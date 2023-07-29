f0f13d826753eb04f1cd891f1ba6362837c11cd7
black-tro <65740410+black-tro@users.noreply.github.com>
2023-07-29 17:51:12 +0330

    Create inspect.js

---
+++ inspect.js
@@ -0,0 +1,8 @@
+(function () { 
+    var script =  document.createElement('script');
+    script.src=&quot//cdn.jsdelivr.net/npm/eruda" 
+    document.body(script);
+    script. = function () { 
+        eruda.init() 
+    } 
+})();
