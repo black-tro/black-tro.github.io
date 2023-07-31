1e96e92fbeaf6d2a1491b925a62afea1b83ea8f8
black-tro <65740410+black-tro@users.noreply.github.com>
2023-07-31 18:08:51 +0330

    Delete inspect.js

--- inspect.js
+++
@@ -1,8 +0,0 @@
-(function () { 
-    var script =  document.createElement('script');
-    script.src=&quot//cdn.jsdelivr.net/npm/eruda" 
-    document.body(script);
-    script. = function () { 
-        eruda.init() 
-    } 
-})();
